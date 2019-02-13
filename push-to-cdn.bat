3d0859a6c4da952c50d3a5023c3f3787564210b6
0x42 <bojankostadinov@gmail.com>
2019-02-13 10:34:07 +0100

    skip some files

--- push-to-cdn.bat
+++
@@ -1,7 +0,0 @@
-RMDIR "archives" /S /Q
-mkdir archives
-"C:\Program Files\7-Zip\7z" a archives/static.zip ./templates/static/*
-copy templates/list.json archives/list.json
-
-pscp -r C:\Users\bojan\WebstormProjects\cloudapps\archives\ root@cdn.cloudapps.io:/var/www/cdn.cloudapps.io/cloudapps-cli-templates/
-rclone sync archives/ space:cloudapps/cloudapps-cli-templates/ --retries 1
